eaae82d7f13c86ee9f5ad50da89155e1d93f7a1c
ZeronYuan <zeronyuan@outlook.com>
2018-12-16 18:24:02 +0800

    github

--- src/plugins/http/apiList.js
+++ src/plugins/http/apiList.js
@@ -2,6 +2,6 @@ export default {
   /**
    * 接口列表
    */
-  TEST1: '/service/getIpInfo.php',
-  TEST2: '/likePoetry', // 搜索古诗词作者
+  TEST1: '/service/getIpInfo.php', // IP ip: '63.223.108.42'
+  TEST2: '/likePoetry', // 搜索古诗词作者 name: '李白'
 };
