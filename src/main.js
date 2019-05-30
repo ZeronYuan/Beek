c7690693e5d569cee6b433b6e0d5c5f1c43015f3
zeron <zeronyuan@outlook.com>
2019-05-30 16:41:19 +0800

    项目依赖更新

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ window.onload = () => {
   const loadDom = document.querySelector('#loading');
   setTimeout(() => {
     loadDom.className = 'fadeOutDown animated';// flipOutY
-  }, 2000 * 600);
+  }, 2000);
 };
 new Vue({
   router,
